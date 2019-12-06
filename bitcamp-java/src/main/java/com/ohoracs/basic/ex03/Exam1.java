164cb69c1d0a4e985a2060c96efd4f0e7e405c30
eunjeong <euuun22@naver.com>
2019-12-06 19:35:33 +0900

    self-checklist

--- bitcamp-java/src/main/java/com/ohoracs/basic/ex03/Exam1.java
+++ bitcamp-java/src/main/java/com/ohoracs/basic/ex03/Exam1.java
@@ -3,6 +3,8 @@ package com.ohoracs.basic.ex03;
 //literal
 public class Exam1 {
   public static void main(String[] args) {
+    //데이터는 크게 아래 5개이다
+
     //정수 리터럴
     System.out.println(78); //10진수 표시법
     
@@ -15,7 +17,24 @@ public class Exam1 {
     System.out.println(0x4e); //16진수 표시법
     System.out.println(-0x4e); //16진수 표시법
 
-    System.out.println(0b01001110); //
+    System.out.println(0b01001110); //8진수 표시법
+
+    //부동소수점 리터럴
+    System.out.println(3.14); //고정소수점
+    System.out.println(0.314e1); //부동소수점 e1에 10의1승을 곱해라 0.314*10
+    System.out.println(31.4e-1); //부동소수점 값은 같다
+
+    //논리 리터럴
+    System.out.println(true); //참
+    System.out.println(false); //거짓 java는 대소문자를 구분한다
+
+    //문자 리터럴
+    System.out.println('a'); //문자 한개를 표현할때 싱글''
+    System.out.println('가');
+
+    //문자열 리터럴
+    System.out.println("안녕하세요! 비트캠프!");
+
 
   }
 }
