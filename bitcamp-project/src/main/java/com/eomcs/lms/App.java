0e4e80a9931873fa0e51e8cb279700aaab3f3963
eunjeong <euuun22@naver.com>
2019-12-20 19:42:20 +0900

    .

--- bitcamp-project/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-project/src/main/java/com/eomcs/lms/App.java
@@ -4,7 +4,6 @@ import java.sql.Date;
 import java.util.Scanner;
 
 public class App {
-
   static Scanner keyboard = new Scanner(System.in);
 
   static class Lesson {
@@ -16,11 +15,11 @@ public class App {
     int totalHours;
     int dayHours;
   }
+  
   static final int LESSON_SIZE = 100;
   static Lesson[] lessons = new Lesson[LESSON_SIZE];
   static int lessonCount = 0;
-
-
+  
   static class Member {
     int no;
     String name;
@@ -30,71 +29,76 @@ public class App {
     String tel;
     Date registeredDate;
   }
+  
+  
   static final int MEMBER_SIZE = 100; 
   static Member[] members = new Member[MEMBER_SIZE];
   static int memberCount = 0;
-
+  
   static class Board { 
     int no;
     String title;
     Date date;
     int viewCount;
   }
-
+  
   static final int BOARD_SIZE = 100;
   static Board[] boards = new Board[BOARD_SIZE];
   static int boardCount = 0;
-
+  
+  
+  
+  
   public static void main(String[] args) {
 
 
     String command;
-
+    
     do {
       System.out.print("\n명령> ");
       command = keyboard.nextLine();
-
+      
       switch (command) {
         case "/lesson/add":
           addLesson();
-
+          
           break;
         case "/lesson/list":
           listLesson();
-
+          
           break;
         case "/member/add":
           addMember();
-
+          
           break;
         case "/member/list":
           listMember();
-
+          
           break;
         case "/board/add":
           addBoard();
-
           break;
         case "/board/list":
           listBoard();
-
-
+          
+          
         default:
           if (!command.equalsIgnoreCase("quit")) {
             System.out.println("실행할 수 없는 명령입니다.");
           }
       }
-
+      
     } while (!command.equalsIgnoreCase("quit"));
-
+    
     System.out.println("안녕!");
-
+    
     keyboard.close();
   }
-
-  static void addLesson() {
+  
+  
+  static void addLesson(){
     Lesson lesson = new Lesson();
-
+    
     System.out.print("번호? ");
     lesson.no = keyboard.nextInt();
 
@@ -118,12 +122,12 @@ public class App {
     System.out.print("일수업시간? ");
     lesson.dayHours = keyboard.nextInt();
     keyboard.nextLine(); 
-
+    
     lessons[lessonCount++] = lesson;
     System.out.println("저장하였습니다.");
+    
   }
-
-
+  
   static void listLesson() {
     for (int i = 0; i < lessonCount; i++) {
       Lesson l = lessons[i];
@@ -131,14 +135,14 @@ public class App {
           l.no, l.title, l.startDate, l.endDate, l.totalHours);
     }
   }
-
+  
   static void addMember() {
     Member member = new Member();        
-
+    
     System.out.print("번호?: ");
     member.no = keyboard.nextInt();
     keyboard.nextLine();
-
+ 
     System.out.print("이름?: ");
     member.name = keyboard.nextLine();
 
@@ -158,19 +162,20 @@ public class App {
 
     members[memberCount++] = member;
     System.out.println("저장하였습니다.");
-
+         
   }
+  
   static void listMember() {
     for (int i = 0; i < memberCount; i++) {  
       Member m = members[i];
-      System.out.printf("%d, %s, %s,  %s, %s\n", 
-          m.no, m.name, m.email, m.tel, m.registeredDate);
-    }
-  } 
-
-  static void addBoard() { 
+    System.out.printf("%d, %s, %s,  %s, %s\n", 
+        m.no, m.name, m.email, m.tel, m.registeredDate);
+    }    
+  }
+  
+  static void addBoard() {
     Board board = new Board();  
-
+    
     System.out.print("번호? ");
     board.no = keyboard.nextInt();
     keyboard.nextLine();
@@ -180,21 +185,20 @@ public class App {
 
     board.date = new Date(System.currentTimeMillis());
     board.viewCount = 0;
-
-
+    
+    
     boards[boardCount++] = board;
-    System.out.println("저장하였습니다.");    
-
+    System.out.println("저장하였습니다.");          
+    
   }
-
+  
   static void listBoard() {
-    for (int i = 0; i < memberCount; i++) {  
-      Member m = members[i];
-      System.out.printf("%d, %s, %s,  %s, %s\n", 
-          m.no, m.name, m.email, m.tel, m.registeredDate);
-
+    for (int i = 0; i < boardCount; i++) {
+      Board b = boards[i];
+      System.out.printf("%d, %s, %s, %d\n", 
+          b.no, b.title, b.date, b.viewCount);
     }
-
+    
   }
-
+  
 }
