f5eca3843345c9ce3634eff89a97e596dca7a3a5
eunjeong <euuun22@naver.com>
2019-12-22 21:06:51 +0900

    .

--- bitcamp-java/src/main/java/com/eomcs/basic/ex07/assignment/Test02.java
+++ bitcamp-java/src/main/java/com/eomcs/basic/ex07/assignment/Test02.java
@@ -1,5 +1,7 @@
 package com.eomcs.basic.ex07.assignment;
 
+import java.util.Arrays;
+
 public class Test02 {
   public static void main(String[] args) {
     int[] values = {34, 4, -3, 78, 12, 22, 45, 0, -22};
@@ -13,7 +15,12 @@ public class Test02 {
     // 출력결과:
     // -22, -3, 0, 4, 12, 22, 34, 45, 78
   }
-  
+  static void sort(int[] values) {
+	  
+	  Arrays.sort(values);
+
+	  
+  }
   
   
 }
