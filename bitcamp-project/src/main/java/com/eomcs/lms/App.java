96deadbc690e5adf0c70f36ed3e9ffcfff61cd47
eunjeong <euuun22@naver.com>
2020-01-21 17:18:53 +0900

    v26 중간완성

--- bitcamp-project/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-project/src/main/java/com/eomcs/lms/App.java
@@ -11,15 +11,29 @@ import java.util.Scanner;
 import com.eomcs.lms.domain.Board;
 import com.eomcs.lms.domain.Lesson;
 import com.eomcs.lms.domain.Member;
-import com.eomcs.lms.handler.BoardHandler;
-import com.eomcs.lms.handler.LessonHandler;
-import com.eomcs.lms.handler.MemberHandler;
+import com.eomcs.lms.handler.BoardAddCommand;
+import com.eomcs.lms.handler.BoardDeleteCommand;
+import com.eomcs.lms.handler.BoardDetailCommand;
+import com.eomcs.lms.handler.BoardListCommand;
+import com.eomcs.lms.handler.BoardUpdateCommand;
+import com.eomcs.lms.handler.Command;
+import com.eomcs.lms.handler.LessonAddCommand;
+import com.eomcs.lms.handler.LessonDeleteCommand;
+import com.eomcs.lms.handler.LessonDetailCommand;
+import com.eomcs.lms.handler.LessonListCommand;
+import com.eomcs.lms.handler.LessonUpdateCommand;
+import com.eomcs.lms.handler.MemberAddCommand;
+import com.eomcs.lms.handler.MemberDeleteCommand;
+import com.eomcs.lms.handler.MemberDetailCommand;
+import com.eomcs.lms.handler.MemberListCommand;
+import com.eomcs.lms.handler.MemberUpdateCommand;
 import com.eomcs.util.Prompt;
 
 
 public class App {
 
   static Scanner keyboard = new Scanner(System.in);
+
   static Deque<String> commandStack = new ArrayDeque<>();
   static Queue<String> commandQueue = new LinkedList<>();
 
@@ -27,37 +41,26 @@ public class App {
 
     Prompt prompt = new Prompt(keyboard);
 
-    // 단지 유지보수를 좋게 하기 위해 ArrayList와 LinkedList의 공통 분모를 뽑아서
-    // 만든 클래스가 List이다.
-    // List의 클래스는 실제 작업을 하는 클래스가 아니다
-    // 그럼에도 불구하고 개발자가 다음과 같이 List객체를 사용하려 한다면 막을 수 없다.
-    // => 실행할 때 오류가 발생할 것이다
-    // => BoardHandler의 경우 아무런 작업을 수행하지 않을 것이다.
-    // 왜? List 클래스에 정으된 메서드는 아무것도 하지 않는다.
-    //
-    //List<Board> boardList = new List<>();
-    //
-    // 해결책?
-    // => generalization 을 통해 만든 클래스의 경우 서브 클래스에게 공통 분모를 물려주기 위한 
-    //    용도로 사용된다.
-    // => 이런 류의 클래스는 직접 인스턴스를 생성하지 못하도록 해서 직접 사용하는 것을 막아야한다.
-    // => 이런 용도로 사용하는 문법이 "추상클래스(Abstract Class)"이다
-    // List 클래스(AbstractList로a 이름 변경함)를 추상 클래스로 만들면,
-    // 다음과 같이 인스턴스를 생성 할 수 없다.
-    // 아예 인스턴스 생성을 원천적으로 차단하는 효과가 있다.
-    //
-    //  AbstractList<Board> boardList = new AbstractList<>(); //컴파일오류! 인스턴스 생성 놉!
-    //
-    //
-    
     LinkedList<Board> boardList = new LinkedList<>();
-    BoardHandler boardHandler = new BoardHandler(prompt, boardList);
-    
+    Command boardAddCommand = new BoardAddCommand(prompt, boardList);
+    Command boardListCommand = new BoardListCommand(boardList);
+    Command boardDetailCommand = new BoardDetailCommand(prompt, boardList);
+    Command boardUpdateCommand = new BoardUpdateCommand(prompt, boardList);
+    Command boardDeleteCommand = new BoardDeleteCommand(prompt, boardList);
+
     ArrayList<Lesson> lessonList = new ArrayList<>();
-    LessonHandler lessonHandler = new LessonHandler(prompt, lessonList);
-    
+    Command lessonAddCommand = new LessonAddCommand(prompt, lessonList);
+    Command lessonListCommand = new LessonListCommand(lessonList);
+    Command lessonDetailCommand = new LessonDetailCommand(prompt, lessonList);
+    Command lessonUpdateCommand = new LessonUpdateCommand(prompt, lessonList);
+    Command lessonDeleteCommand = new LessonDeleteCommand(prompt, lessonList);
+
     LinkedList<Member> memberList = new LinkedList<>();
-    MemberHandler memberHandler = new MemberHandler(prompt, memberList);
+    Command memberAddCommand = new MemberAddCommand(prompt, memberList);
+    Command memberListCommand = new MemberListCommand(memberList);
+    Command memberDetailCommand = new MemberDetailCommand(prompt, memberList);
+    Command memberUpdateCommand = new MemberUpdateCommand(prompt, memberList);
+    Command memberDeleteCommand = new MemberDeleteCommand(prompt, memberList);
 
     String command;
 
@@ -74,53 +77,53 @@ public class App {
 
       switch (command) {
         case "/lesson/add":
-          lessonHandler.addLesson();
+          lessonAddCommand.execute();
           break;
         case "/lesson/list":
-          lessonHandler.listLesson();
-          break;        
+          lessonListCommand.execute();
+          break;
         case "/lesson/detail":
-          lessonHandler.detailLesson();
+          lessonDetailCommand.execute();
           break;
         case "/lesson/update":
-          lessonHandler.updateLesson();
+          lessonUpdateCommand.execute();
           break;
         case "/lesson/delete":
-          lessonHandler.deleteLesson();
+          lessonDeleteCommand.execute();
           break;
         case "/member/add":
-          memberHandler.addMember();
+          memberAddCommand.execute();
           break;
         case "/member/list":
-          memberHandler.listMember();
+          memberListCommand.execute();
           break;
         case "/member/detail":
-          memberHandler.detailMember();
+          memberDetailCommand.execute();
           break;
         case "/member/update":
-          memberHandler.updateMember();
+          memberUpdateCommand.execute();
           break;
         case "/member/delete":
-          memberHandler.deleteMember();
+          memberDeleteCommand.execute();
           break;
         case "/board/add":
-          boardHandler.addBoard();
+          boardAddCommand.execute();
           break;
         case "/board/list":
-          boardHandler.listBoard();
+          boardListCommand.execute();
           break;
         case "/board/detail":
-          boardHandler.detailBoard();
+          boardDetailCommand.execute();
           break;
         case "/board/update":
-          boardHandler.updateBoard();
+          boardUpdateCommand.execute();
           break;
         case "/board/delete":
-          boardHandler.deleteBoard();
+          boardDeleteCommand.execute();
           break;
         case "history":
           printCommadHistory(commandStack.iterator());
-          break; 
+          break;
         case "history2":
           printCommadHistory(commandQueue.iterator());
           break;
@@ -136,16 +139,11 @@ public class App {
 
     keyboard.close();
   }
-  
-  
-  // 이전에는 Stack에서 값을 꺼내는 방법과 Queue에서 값을 꺼내는 방법이 다르기 때문에
-  // printCommandSHistory()와 2 메서드를 따로 정의했다
-  // 이제 스택과 큐는 일관된 방식으로 값을 꺼내주는 Iterator가 있기 때문에
-  // 두 메서드를 하나로 합칠 수 있다.
-  // 파라미터로 Iterator를 받아서 처리하기만 하면 된다.
+
+
   private static void printCommadHistory(Iterator<String> iterator) {
     int count = 0;
-    
+
     while (iterator.hasNext()) {
       System.out.println(iterator.next());
       count++;
@@ -161,11 +159,7 @@ public class App {
   }
 
 
-  
 
 }
-
-
-
 
 
