129a00f2e44f7b2013241cb096f6afe66f42435f
eunjeong <euuun22@naver.com>
2020-02-16 17:06:16 +0900

    2020-02-16

--- bitcamp-project-client/src/main/java/com/eomcs/lms/handler/BoardListCommand.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/handler/BoardListCommand.java
@@ -26,15 +26,18 @@ public class BoardListCommand implements Command {
       // JDBC Driver(MariaDB 프록시) 로딩
       Class.forName("org.mariadb.jdbc.Driver");
 
-
+      // JDBC Driver를 이용하여 MariaDB 에 접속한다.
       Connection con =
-          DriverManager.getConnection("jdbc:mariadb://localhost:3306/studydb", "study", "1111");
-
+          DriverManager.getConnection("jdbc:mariadb://localhost/studydb", "study", "1111");
+      
+      // MariaDB에 명령을 전달할 객체 준비
       Statement stmt = con.createStatement();
-
+      
+      // MariaDB의 lms_board 테이블에 있는 데이터를 가져올 도구를 준비
       ResultSet rs = stmt.executeQuery("select board_id, conts, cdt, vw_cnt from lms_board");
-
-      while (rs.next()) {
+      
+       // ResultSet 도구를 사용하여 데이터를 하나씩 가져온다.
+      while (rs.next()) { // 데이터를 한 개 가져왔으면 true를 리턴한다.
         System.out.printf("%d, %s, %s, %d\n", rs.getInt("board_id"), rs.getString("conts"),
             rs.getDate("cdt"), rs.getInt("vw_cnt"));
 
