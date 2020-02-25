d8e7f5092c01dd82766ea1304be7b734b0bd082b
eunjeong <euuun22@naver.com>
2020-02-25 14:55:24 +0900

    2020-02-25 /photoBoard/첨부파일 추가

---
+++ bitcamp-project-server/v38_2/src/main/java/com/eomcs/lms/DataLoaderListener.java
@@ -0,0 +1,49 @@
+package com.eomcs.lms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
+import com.eomcs.lms.context.ApplicationContextListener;
+import com.eomcs.lms.dao.mariadb.BoardDaoImpl;
+import com.eomcs.lms.dao.mariadb.LessonDaoImpl;
+import com.eomcs.lms.dao.mariadb.MemberDaoImpl;
+import com.eomcs.lms.dao.mariadb.PhotoBoardDaoImpl;
+import com.eomcs.lms.dao.mariadb.PhotoFileDaoImpl;
+
+// 애플리케이션이 시작되거나 종료될 때
+// 데이터를 로딩하고 저장하는 일을 한다.
+//
+public class DataLoaderListener implements ApplicationContextListener {
+
+  Connection con;
+
+  @Override
+  public void contextInitialized(Map<String, Object> context) {
+
+    try {
+      // DB 연결객체 준비
+      Class.forName("org.mariadb.jdbc.Driver");
+      con = DriverManager.getConnection("jdbc:mariadb://localhost/studydb", "study", "1111");
+
+
+      context.put("boardDao", new BoardDaoImpl(con));
+      context.put("lessonDao", new LessonDaoImpl(con));
+      context.put("memberDao", new MemberDaoImpl(con));
+      context.put("photoBoardDao", new PhotoBoardDaoImpl(con));
+      context.put("photoFileDao", new PhotoFileDaoImpl(con));
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void contextDestroyed(Map<String, Object> context) {
+    try {
+      con.close();
+    } catch (Exception e) {
+      // DB 커넥션을 닫다가 예외 발생하면 무시한다.
+      // 왜? 클라이언트 쪽에서 달리 해야 할 일이 없다.
+    }
+  }
+}
---
+++ bitcamp-project-server/v38_2/src/main/java/com/eomcs/lms/servlet/PhotoBoardDeleteServlet.java
@@ -0,0 +1,38 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import com.eomcs.lms.dao.PhotoBoardDao;
+import com.eomcs.lms.dao.PhotoFileDao;
+
+public class PhotoBoardDeleteServlet implements Servlet {
+
+  PhotoBoardDao photoBoardDao;
+  PhotoFileDao photoFileDao;
+
+  public PhotoBoardDeleteServlet(PhotoBoardDao photoBoardDao, PhotoFileDao photoFileDao) {
+    this.photoBoardDao = photoBoardDao;
+    this.photoFileDao = photoFileDao;
+  }
+
+
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+    out.println("번호? ");
+    out.println("!{}!");
+    out.flush();
+
+    int no = Integer.parseInt(in.nextLine());
+
+    // 사진 게시글을 삭제전 먼저 자식테이블에 있는 첨부파일 데이터를 삭제한다.
+    photoFileDao.deleteAll(no);
+
+    if (photoBoardDao.delete(no) > 0) {
+      out.println("사진 게시글을 삭제했습니다.");
+
+    } else {
+      out.println("해당 번호의 사진 게시글이 없습니다.");
+    }
+  }
+}
