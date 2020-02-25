da1ca638f2cf71db366678022402b975cbe19fe9
eunjeong <euuun22@naver.com>
2020-02-25 17:40:59 +0900

    2020-02-25 최종

---
+++ bitcamp-project-server/v38_4/src/main/java/com/eomcs/lms/dao/PhotoBoardDao.java
@@ -0,0 +1,19 @@
+package com.eomcs.lms.dao;
+
+import java.util.List;
+import com.eomcs.lms.domain.PhotoBoard;
+
+public interface PhotoBoardDao {
+  int insert(PhotoBoard photoboard) throws Exception;
+
+  List<PhotoBoard> findAllByLessonNo(int lessonNo) throws Exception;
+
+  PhotoBoard findByNo(int no) throws Exception;
+
+  int update(PhotoBoard photoBoard) throws Exception;
+
+  int delete(int no) throws Exception;
+
+
+
+}
