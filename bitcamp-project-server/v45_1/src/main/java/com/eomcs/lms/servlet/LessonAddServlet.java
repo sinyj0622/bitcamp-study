4bec6fb45208f3562a8f8f5d4f6849048b46b759
eunjeong <euuun22@naver.com>
2020-03-13 14:35:42 +0900

    2020-03-13

---
+++ bitcamp-project-server/v45_1/src/main/java/com/eomcs/lms/servlet/LessonAddServlet.java
@@ -0,0 +1,42 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.lms.service.LessonService;
+import com.eomcs.util.Prompt;
+
+public class LessonAddServlet implements Servlet {
+
+
+  LessonService lessonService;
+
+  public LessonAddServlet(LessonService lessonService) {
+    this.lessonService = lessonService;
+  }
+
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception { // 예외를 던짐
+
+    Lesson lesson = new Lesson();
+
+    lesson.setTitle(Prompt.getString(in, out, "수업명? "));
+    lesson.setDescription(Prompt.getString(in, out, "설명? "));
+    lesson.setStartDate(Prompt.getDate(in, out, "시작일? "));
+    lesson.setEndDate(Prompt.getDate(in, out, "종료일? "));
+    lesson.setTotalHours(Prompt.getInt(in, out, "총수업시간? "));
+    lesson.setDayHours(Prompt.getInt(in, out, "일수업시간? "));
+    out.flush();
+
+    if (lessonService.add(lesson) > 0) {
+      out.println("새 수업을 등록했습니다!");
+
+    } else {
+      out.println("수업 등록을 실패하였습니다.");
+
+    }
+
+  }
+
+}
---
+++ bitcamp-project-server/v45_1/src/main/java/com/eomcs/lms/servlet/PhotoBoardListServlet.java
@@ -0,0 +1,56 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.lms.domain.PhotoBoard;
+import com.eomcs.lms.service.LessonService;
+import com.eomcs.lms.service.PhotoBoardService;
+
+public class PhotoBoardListServlet implements Servlet {
+
+  PhotoBoardService photoBoardService;
+  LessonService lessonService;
+
+  public PhotoBoardListServlet(PhotoBoardService photoBoardService, LessonService lessonService) {
+    this.photoBoardService = photoBoardService;
+    this.lessonService = lessonService;
+  }
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+    out.println("수업번호? ");
+    out.println("!{}!");
+    out.flush();
+
+    int lessonNo = Integer.parseInt(in.nextLine());
+
+    // 1) 수업 번호로 수업 정보를 가져온다.
+    Lesson lesson = lessonService.get(lessonNo);
+
+    // 2) 수업 번호에 해당하는 수업을 못찾았다면,
+    // 안내 문구를 출력하고 응답을 종료한다.
+    if (lesson == null) {
+      out.println("수업 번호가 유효하지 않습니다.");
+      return;
+    }
+
+    // 3) 수업 번호에 해당하는 수업을 찾았다면, 수업명을 출력한다.
+    out.printf("수업명: %s\n", lesson.getTitle());
+    out.println("----------------------------------------------------------");
+
+    // 4) 해당 수업의 사진 게시글을 가져온다.
+    List<PhotoBoard> photoBoards = photoBoardService.listLessonPhoto(lessonNo);
+
+    // 5) 클라이언트에게 게시글을 출력한다.
+    for (PhotoBoard photoBoard : photoBoards) {
+      out.printf("%d, %s, %s, %d\n", //
+          photoBoard.getNo(), //
+          photoBoard.getTitle(), //
+          photoBoard.getCreatedDate(), //
+          photoBoard.getViewCount() //
+      );
+    }
+  }
+}
