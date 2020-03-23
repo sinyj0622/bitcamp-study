28eae1afa29675e769b783dd2d6c3c9ac48d0c9f
eunjeong <euuun22@naver.com>
2020-03-23 11:03:18 +0900

    2020-03-23

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex09/a/Exam01.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex09/a/Exam01.java
@@ -1,25 +1,20 @@
 // 객체 자동 생성 - @Component 애노테이션
-package com.eomcs.spring.ioc.ex09;
+package com.eomcs.spring.ioc.ex09.a;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.eomcs.spring.ioc.SpringUtils;
 
 public class Exam01 {
 
   public static void main(String[] args) {
-    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-        "bitcamp/java106/step09/application-context-01.xml");
+    ApplicationContext iocContainer =
+        new ClassPathXmlApplicationContext("com/eomcs/spring/ioc/ex09/a/application-context.xml");
 
-    SpringUtils.printBeanNames(iocContainer);
+    SpringUtils.printBeanList(iocContainer);
 
-    System.out.println(iocContainer.getBean("car"));
   }
 
 }
-
-
-
 
 
--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex09/c/Exam03.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex09/c/Exam03.java
@@ -1,23 +1,19 @@
 // 객체 자동 생성 - 특정 패키지의 클래스에 대해 객체 생성하지 않기
-package com.eomcs.spring.ioc.ex09;
+package com.eomcs.spring.ioc.ex09.c;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.eomcs.spring.ioc.SpringUtils;
 
 public class Exam03 {
 
   public static void main(String[] args) {
-    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-        "bitcamp/java106/step09/application-context-03.xml");
+    ApplicationContext iocContainer =
+        new ClassPathXmlApplicationContext("com/eomcs/spring/ioc/ex09/c/application-context.xml");
 
-    SpringUtils.printBeanNames(iocContainer);
+    SpringUtils.printBeanList(iocContainer);
   }
 
 }
-
-
-
 
 
