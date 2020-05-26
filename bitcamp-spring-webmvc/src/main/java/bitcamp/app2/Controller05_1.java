cee8a650ae1ed1760a31054850041efa63c6dc1e
eunjeong <euuun22@naver.com>
2020-05-26 12:42:32 +0900

    2020-05-26

--- bitcamp-spring-webmvc/src/main/java/bitcamp/app2/Controller05_1.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/app2/Controller05_1.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.google.gson.Gson;
 
-@Controller 
+@Controller
 @RequestMapping("/c05_1")
 public class Controller05_1 {
 
   ArrayList<Board> list = new ArrayList<>();
-  
+
   public Controller05_1() {
     list.add(new Board(1, "제목입니다1", "내용", "홍길동", 10, Date.valueOf("2019-5-1")));
     list.add(new Board(2, "제목입니다2", "내용", "홍길동2", 11, Date.valueOf("2019-5-2")));
@@ -31,40 +31,42 @@ public class Controller05_1 {
     list.add(new Board(12, "제목입니다12", "내용", "홍길동12", 12, Date.valueOf("2019-9-1")));
     list.add(new Board(13, "제목입니다13", "내용", "홍길동13", 13, Date.valueOf("2019-10-1")));
   }
-  
+
   // 1) JSP에서 JSON 형식의 콘텐트 출력하기
-  // 테스트:
-  //   http://.../app2/c05_1/h1
+  // 직접 테스트:
+  // http://.../app2/c05_1/h1
+  // HTML에서 AJAX 요청 테스트:
+
   @GetMapping("h1")
   public void handler1(Model model) {
     model.addAttribute("list", this.list);
   }
-  
+
   // 2) Google Gson 라이브러리를 사용하여 JSON 형식의 콘텐트 출력하기
   // => mvnrepository.com에서 gson 검색하여 라이브러리 정보를 가져온다.
   // => build.gradle 파일에 추가한다.
   // => '$gradle eclipse' 실행한다.
   // => 이클립스에서 프로젝트를 리프래시 한다.
   // 테스트:
-  //   http://.../app2/c05_1/h2
-  @GetMapping(value="h2", produces="text/plain;charset=UTF-8")
-  @ResponseBody
+  // http://.../app2/c05_1/h2
+  @GetMapping(value = "h2", produces = "text/plain;charset=UTF-8")
+  @ResponseBody // jsp가 아니라 클라이언트에게 바로출력하는 콘텐츠 설정
   public String handler2() {
     return new Gson().toJson(this.list);
   }
-  
-  
+
+
   // 3) Google Gson 라이브러리를 사용하여 JSON 형식의 콘텐트 출력하기 II
-  // => 페이지 컨트롤러의 리턴 값이 String이 아니면 
-  //    프론트 컨트롤러는 
-  //    Google의 Gson 라이브러리나 Jackson 라이브러리를 사용하여 
-  //    자동으로 JSON 형식의 문자열로 만들어 클라이언트로 출력한다.
+  // => 페이지 컨트롤러의 리턴 값이 String이 아니면
+  // 프론트 컨트롤러는
+  // Google의 Gson 라이브러리나 Jackson 라이브러리를 사용하여
+  // 자동으로 JSON 형식의 문자열로 만들어 클라이언트로 출력한다.
   // => 단 Gson 또는 Jackson 라이브러리가 있어야 한다.
-  //    둘 다 있다면 Jackson 라이브러리가 기본으로 사용된다.
-  // => build.gradle 파일에서 gson 또는 jackson 라이브러리를 추가하는 부분의 
-  //    주석을 참고하라!
+  // 둘 다 있다면 Jackson 라이브러리가 기본으로 사용된다.
+  // => build.gradle 파일에서 gson 또는 jackson 라이브러리를 추가하는 부분의
+  // 주석을 참고하라!
   // 테스트:
-  //   http://.../app2/c05_1/h3
+  // http://.../app2/c05_1/h3
   @GetMapping("h3")
   @ResponseBody
   public Object handler3() {
@@ -72,16 +74,5 @@ public class Controller05_1 {
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
--- bitcamp-spring-webmvc/src/main/webapp/WEB-INF/jsp2/c05_1/h1.jsp
+++ bitcamp-spring-webmvc/src/main/webapp/WEB-INF/jsp2/c05_1/h1.jsp
@@ -3,13 +3,14 @@
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 [
-<c:forEach items="${list}" var="b">
+<c:forEach items="${list}" var="b" varStatus="status">
   {
     "no":${b.no}, 
     "title":"${b.title}", 
     "writer":"${b.writer}", 
     "viewCount":${b.viewCount},
     "createdDate":"${b.createdDate}"
-  },
+  }
+  <c:if test="${!status.last}">,</c:if>
 </c:forEach>
 ]
